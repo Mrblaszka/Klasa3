8027fcdacc69b520781d5439a37d148a1983411c
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-09-13 10:58:15 +0200

    Finished project

--- app/src/main/java/com/example/klasa3_1/MainActivity.kt
+++ app/src/main/java/com/example/klasa3_1/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.klasa3_1
+    package com.example.klasa3_1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var text = findViewById<TextView>(R.id.tekst1)
+        findViewById<Button>(R.id.button3).setBackgroundColor(Color.YELLOW);
     }
 }
